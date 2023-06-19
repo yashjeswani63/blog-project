5b16b772b403da36aec7a41f6c50fafd45b77ddc
Yash Jeswani <yashjeswani63@gmail.com>
2023-06-19 15:19:46 +0530

    pushed

---
+++ src/BlogApp/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+function Footer() {
+  return (
+    <div className='footer'>
+        <h2>Blogging Application</h2>
+        <p>@copyright code</p>
+    </div>
+  )
+}
+
+export default Footer
